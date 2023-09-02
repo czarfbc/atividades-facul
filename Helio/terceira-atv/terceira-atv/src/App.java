03fca1e1bdc3049e724103405fe5a42ea67626d6
Cezar Fernando <cezarfbc@gmail.com>
2023-09-02 11:17:46 -0300

    .

---
+++ Helio/terceira-atv/terceira-atv/src/App.java
@@ -0,0 +1,155 @@
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // ATV-1
+        // float km, horas, res;
+        // String sKm, sHoras;
+
+        // sKm = JOptionPane.showInputDialog(null, "Digite a quilometragem");
+        // sHoras = JOptionPane.showInputDialog(null, "Digite as horas");
+
+        // km = Float.parseFloat(sKm);
+        // horas = Float.parseFloat(sHoras);
+
+        // res = km / horas;
+
+        // if (res > 110) {
+        // JOptionPane.showMessageDialog(null, "Ultrapassol o limite " + res);
+        // } else {
+        // JOptionPane.showMessageDialog(null, res);
+        // }
+
+        // ATV-2
+        // float temp;
+        // String sTemp;
+
+        // sTemp = JOptionPane.showInputDialog(null, "digite a temperatura");
+        // temp = Float.parseFloat(sTemp);
+        // if (temp > 36.5) {
+        // JOptionPane.showMessageDialog(null, "febre");
+        // } else {
+        // JOptionPane.showMessageDialog(null, "sem febre");
+        // }
+
+        // ATV-3
+        // int n1, res;
+        // String sn1;
+
+        // sn1 = JOptionPane.showInputDialog(null, "digite um numero");
+        // n1 = Integer.parseInt(sn1);
+        // res = n1 % 7;
+
+        // if (res == 0) {
+        // JOptionPane.showMessageDialog(null, "multiplo de 7");
+        // } else {
+        // JOptionPane.showMessageDialog(null, "não é multiplo de 7");
+        // }
+
+        // ATV-4
+        // int n1, par, div3;
+        // String sn1;
+
+        // sn1 = JOptionPane.showInputDialog(null, "digite um numero");
+        // n1 = Integer.parseInt(sn1);
+
+        // par = n1 % 2;
+        // div3 = n1 % 3;
+
+        // if (par == 0) {
+        // if (div3 == 0) {
+        // JOptionPane.showMessageDialog(null, "par e multiplo de 3");
+        // }
+        // } else {
+        // JOptionPane.showMessageDialog(null, "não satifaz a primeira condiçao de ser
+        // par");
+        // }
+
+        // ATV-5
+        // int n1;
+        // String sn1;
+
+        // sn1 = JOptionPane.showInputDialog(null, "digite um numero");
+        // n1 = Integer.parseInt(sn1);
+
+        // if (n1 > 17 && n1 < 66) {
+        // JOptionPane.showMessageDialog(null, "está entre 18 e 65");
+        // } else {
+        // JOptionPane.showMessageDialog(null, "não está entre 18 e 65");
+        // }
+
+        // ATV-6
+        // String sescolha;
+
+        // sescolha = JOptionPane.showInputDialog(null, "M: masculino \n F: feminino");
+
+        // if (sescolha.equals("M")) {
+        // JOptionPane.showMessageDialog(null, "masculino");
+        // } else if (sescolha.equals("F")) {
+        // JOptionPane.showMessageDialog(null, "feminino");
+        // }
+
+        // ATV-7
+        // float blusa1, blusa2;
+        // String sBlusa1, sBlusa2;
+
+        // sBlusa1 = JOptionPane.showInputDialog(null, "preço blusa 1");
+        // sBlusa2 = JOptionPane.showInputDialog(null, "preço blusa 2");
+
+        // blusa1 = Float.parseFloat(sBlusa1);
+        // blusa2 = Float.parseFloat(sBlusa2);
+
+        // if (blusa1 > blusa2) {
+        // JOptionPane.showMessageDialog(null, "blusa1 mais cara");
+        // } else if (blusa1 < blusa2) {
+        // JOptionPane.showMessageDialog(null, "blusa2 mais cara");
+        // }
+
+        // ATV-8
+        // String volgal;
+
+        // volgal = JOptionPane.showInputDialog(null, "Digite uma letra em ");
+        // volgal = volgal.toLowerCase();
+
+        // if (volgal.equals("a") || volgal.equals("e") || volgal.equals("i") ||
+        // volgal.equals("o")
+        // || volgal.equals("u")) {
+        // JOptionPane.showMessageDialog(null, "volgal");
+        // } else {
+        // JOptionPane.showMessageDialog(null, "não vogal");
+        // }
+
+        // ATV-9
+        // int n1, n2, res;
+        // String sn1, sn2;
+
+        // sn1 = JOptionPane.showInputDialog(null, "digite um numero");
+        // sn2 = JOptionPane.showInputDialog(null, "digite um numero");
+
+        // n1 = Integer.parseInt(sn1);
+        // n2 = Integer.parseInt(sn2);
+
+        // res = n1 % n2;
+
+        // if (res == 0) {
+        // JOptionPane.showMessageDialog(null, n1 + " é multiplo de " + n2);
+        // } else {
+        // JOptionPane.showMessageDialog(null, n1 + " não é multiplo de " + n2);
+        // }
+
+        // ATV-10
+        // int idade, calc;
+        // String sIdade;
+
+        // sIdade = JOptionPane.showInputDialog(null, "digite o ano de nascimento");
+
+        // idade = Integer.parseInt(sIdade);
+        // calc = 2023 - idade;
+
+        // if (calc < 18) {
+        // JOptionPane.showMessageDialog(null, "não pode votar");
+        // } else {
+        // JOptionPane.showMessageDialog(null, "pode votar");
+        // }
+    }
+}
