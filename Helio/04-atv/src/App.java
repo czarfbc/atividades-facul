5c29b8f6c203cb0f4f6cb9fc20bdb7fbb8ef04cd
Cezar Fernando <cezarfbc@gmail.com>
2023-09-08 11:55:59 -0300

    .

--- Helio/04-atv/src/App.java
+++ Helio/04-atv/src/App.java
@@ -2,23 +2,51 @@ import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int idade;
-        String s_idade, sexo;
+        // int idade;
+        // String s_idade, sexo;
 
-        sexo = JOptionPane.showInputDialog(null, "digite o sexo");
-        s_idade = JOptionPane.showInputDialog(null, "digite a idade");
-        idade = Integer.parseInt(s_idade);
+        // sexo = JOptionPane.showInputDialog(null, "digite o sexo");
+        // s_idade = JOptionPane.showInputDialog(null, "digite a idade");
+        // idade = Integer.parseInt(s_idade);
 
-        if (idade > 0 && idade < 100) {
-            if (idade >= 18) {
-                if (sexo.equals("m")) {
-                    JOptionPane.showMessageDialog(null, "masculino > 18");
-                }
-            } else {
-                JOptionPane.showMessageDialog(null, "> 18");
+        // if (idade > 0 && idade < 100) {
+        // if (idade >= 18) {
+        // if (sexo.equals("m")) {
+        // JOptionPane.showMessageDialog(null, "masculino > 18");
+        // }
+        // } else {
+        // JOptionPane.showMessageDialog(null, "> 18");
+        // }
+        // } else {
+        // JOptionPane.showMessageDialog(null, "idade invalida");
+        // }
+
+        int x, a, b, c, par, mediaA, mediaP;
+        String sx, sa, sb, sc;
+
+        sx = JOptionPane.showInputDialog(null, "valor x");
+
+        x = Integer.parseInt(sx);
+
+        par = x % 2;
+
+        if (par == 0) {
+            sa = JOptionPane.showInputDialog(null, "valor a");
+            sb = JOptionPane.showInputDialog(null, "valor b");
+            sc = JOptionPane.showInputDialog(null, "valor c");
+
+            a = Integer.parseInt(sa);
+            b = Integer.parseInt(sb);
+            c = Integer.parseInt(sc);
+            if (x > 0 && x <= 10) {
+                mediaA = (a + b + c) / 3;
+                JOptionPane.showMessageDialog(null, "MediaA: " + mediaA);
+            } else if (x > 10) {
+                mediaP = ((a * 2) + (b * 3) + (c * 4)) / 9;
+                JOptionPane.showMessageDialog(null, "MediaP: " + mediaP);
             }
         } else {
-            JOptionPane.showMessageDialog(null, "idade invalida");
+            JOptionPane.showMessageDialog(null, "Impar");
         }
     }
 }
