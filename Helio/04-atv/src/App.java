4b5a1df529d8f459ed93522ed4b6fb6705e37e4a
Cezar Fernando <czarfbc@gmail.com>
2023-09-14 09:21:27 -0300

    .

--- Helio/04-atv/src/App.java
+++ Helio/04-atv/src/App.java
@@ -21,34 +21,33 @@ public class App {
         // JOptionPane.showMessageDialog(null, "idade invalida");
         // }
 
-        int x, a, b, c, par, mediaA, mediaP;
-        String sx, sa, sb, sc;
+        // int x, a, b, c, par, mediaA, mediaP;
+        // String sx, sa, sb, sc;
 
-        sx = JOptionPane.showInputDialog(null, "valor x");
-        x = Integer.parseInt(sx);
-        par = x % 2;
+        // sx = JOptionPane.showInputDialog(null, "valor x");
+        // x = Integer.parseInt(sx);
+        // par = x % 2;
 
-        while (par != 0) {
-            sx = JOptionPane.showInputDialog(null, "Insira um valor par:");
-            x = Integer.parseInt(sx);
-            par = x % 2;
-        }
+        // while (par != 0) {
+        // sx = JOptionPane.showInputDialog(null, "Insira um valor par:");
+        // x = Integer.parseInt(sx);
+        // par = x % 2;
+        // }
 
-        sa = JOptionPane.showInputDialog(null, "valor a");
-        sb = JOptionPane.showInputDialog(null, "valor b");
-        sc = JOptionPane.showInputDialog(null, "valor c");
+        // sa = JOptionPane.showInputDialog(null, "valor a");
+        // sb = JOptionPane.showInputDialog(null, "valor b");
+        // sc = JOptionPane.showInputDialog(null, "valor c");
 
-        a = Integer.parseInt(sa);
-        b = Integer.parseInt(sb);
-        c = Integer.parseInt(sc);
+        // a = Integer.parseInt(sa);
+        // b = Integer.parseInt(sb);
+        // c = Integer.parseInt(sc);
 
-        if (x > 0 && x <= 10) {
-            mediaA = (a + b + c) / 3;
-            JOptionPane.showMessageDialog(null, "MediaA: " + mediaA);
-        } else if (x > 10) {
-            mediaP = ((a * 2) + (b * 3) + (c * 4)) / 9;
-            JOptionPane.showMessageDialog(null, "MediaP: " + mediaP);
-        }
-
+        // if (x > 0 && x <= 10) {
+        // mediaA = (a + b + c) / 3;
+        // JOptionPane.showMessageDialog(null, "MediaA: " + mediaA);
+        // } else if (x > 10) {
+        // mediaP = ((a * 2) + (b * 3) + (c * 4)) / 9;
+        // JOptionPane.showMessageDialog(null, "MediaP: " + mediaP);
+        // }
     }
 }
---
+++ Helio/05-atv/src/App.java
@@ -0,0 +1,29 @@
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // 1-a
+
+        // 1-b
+        // int sal;
+        // double desc;
+        // String ssal;
+        // ssal = JOptionPane.showInputDialog(null, "digite um salario");
+        // sal = Integer.parseInt(ssal);
+        // if (sal >= 2300 && sal <= 5000) {
+        // desc = sal * 0.925;
+        // JOptionPane.showMessageDialog(null, desc);
+        // } else if (sal > 5000 && sal <= 7000) {
+        // desc = sal * 0.91;
+        // JOptionPane.showMessageDialog(null, desc);
+        // } else if (sal > 7000 && sal <= 9000) {
+        // desc = sal * 0.89;
+        // JOptionPane.showMessageDialog(null, desc);
+        // } else if (sal > 9000) {
+        // desc = sal * 0.86;
+        // JOptionPane.showMessageDialog(null, desc);
+        // } else {
+        // JOptionPane.showMessageDialog(null, "sem desconto");
+        // }
+    }
+}
