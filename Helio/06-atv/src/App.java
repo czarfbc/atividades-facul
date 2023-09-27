0a41b47e1a3cd8abeed3dc6c70b602af35f47f5c
Cezar Fernando <czarfbc@gmail.com>
2023-09-26 22:47:40 -0300

    .

---
+++ Helio/06-atv/src/App.java
@@ -0,0 +1,109 @@
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // ATV-01
+        // int n1, n2, n3;
+        // String sn1, sn2, sn3;
+
+        // sn1 = JOptionPane.showInputDialog(null, "informe um nº");
+        // sn2 = JOptionPane.showInputDialog(null, "informe um nº");
+        // sn3 = JOptionPane.showInputDialog(null, "informe um nº");
+
+        // n1 = Integer.parseInt(sn1);
+        // n2 = Integer.parseInt(sn2);
+        // n3 = Integer.parseInt(sn3);
+
+        // if (n1 > n2 && n1 > n3) {
+        // JOptionPane.showMessageDialog(null, "Maior nº " + n1);
+        // } else if (n2 > n1 && n2 > n3) {
+        // JOptionPane.showMessageDialog(null, "Maior nº " + n2);
+        // } else if (n3 > n1 && n3 > n2) {
+        // JOptionPane.showMessageDialog(null, "Maior nº " + n3);
+        // }
+
+        // ATV-02
+        // int n1, n2, n3;
+        // String sn1, sn2, sn3;
+
+        // sn1 = JOptionPane.showInputDialog(null, "informe um nº");
+        // sn2 = JOptionPane.showInputDialog(null, "informe um nº");
+        // sn3 = JOptionPane.showInputDialog(null, "informe um nº");
+
+        // n1 = Integer.parseInt(sn1);
+        // n2 = Integer.parseInt(sn2);
+        // n3 = Integer.parseInt(sn3);
+
+        // if (n1 == n2 && n1 == n3) {
+        // JOptionPane.showMessageDialog(null, "equilatero");
+        // } else if () {
+        // JOptionPane.showMessageDialog(null, "Maior nº " + n2);
+        // } else if () {
+        // JOptionPane.showMessageDialog(null, "Maior nº " + n3);
+        // }
+
+        // ATV-03
+        // int n1, n2, n3;
+        // String sn1, sn2, sn3;
+
+        // sn1 = JOptionPane.showInputDialog(null, "informe um nº");
+        // sn2 = JOptionPane.showInputDialog(null, "informe um nº");
+        // sn3 = JOptionPane.showInputDialog(null, "informe um nº");
+
+        // n1 = Integer.parseInt(sn1);
+        // n2 = Integer.parseInt(sn2);
+        // n3 = Integer.parseInt(sn3);
+
+        // if (n1 == n2 && n1 == n3) {
+        // JOptionPane.showMessageDialog(null, "equilatero");
+        // } else if () {
+        // JOptionPane.showMessageDialog(null, "Maior nº " + n2);
+        // } else if () {
+        // JOptionPane.showMessageDialog(null, "Maior nº " + n3);
+        // }
+
+        // ATV-04
+        // int n1, n2;
+        // String sn1, sn2;
+
+        // sn1 = JOptionPane.showInputDialog(null, "informe um nº");
+        // sn2 = JOptionPane.showInputDialog(null, "informe um nº");
+
+        // n1 = Integer.parseInt(sn1);
+        // n2 = Integer.parseInt(sn2);
+
+        // if (n1 <= 3) {
+        // JOptionPane.showMessageDialog(null, (n1 * 4) * 0.95);
+        // } else {
+        // JOptionPane.showMessageDialog(null, (n1 * 4) * 0.9);
+        // }
+
+        // if (n2 <= 3) {
+        // JOptionPane.showMessageDialog(null, (n2 * 3) * 0.93);
+        // } else {
+        // JOptionPane.showMessageDialog(null, (n2 * 3) * 0.85);
+        // }
+
+        // DECLARÇÃO DE VARIÁVEIS
+        int p1, p2;
+        float valor, p3;
+        String s_valor;
+
+        // PEGAR VALOR INFORMADO PELO USUÁRIO
+        s_valor = JOptionPane.showInputDialog(null, "Informe um número:");
+
+        // TRANSFORMAR TEXTO EM UM Nº
+        valor = Float.parseFloat(s_valor);
+
+        // PEGAR A PARTE INTEIRA DA DIVISÃO
+        p1 = (int) (valor / 3);
+        p2 = (int) (valor / 3);
+
+        // O RESTANTE DO VALOR
+        p3 = valor - p1 * 2;
+
+        // MOSTRAR NA TELA DE MANEIRA FORMATADA
+        JOptionPane.showMessageDialog(null, String.format("p1 = %d, p2 = %d, p3 = %.2f", p1, p2, p3));
+
+    }
+}
